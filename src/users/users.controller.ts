293bfff54945e65cfa984d7bb5cedd489561f543
Damhafi <yusufmilla@gmail.com>
2022-02-25 15:16:06 -0300

    att.

---
+++ src/users/users.controller.ts
@@ -0,0 +1,43 @@
+import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from '@prisma/client'
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  @Post()
+  async create(@Body() UserData: {name: string, email:string}) : Promise<User> {
+    const {name, email} = UserData
+    return this.usersService.create({name, email});
+  }
+
+  @Get()
+  async findAll() : Promise<User[]> {
+    return this.usersService.findAll();
+  }
+
+  @Get(':id')
+  async findOne(@Param('id') id: string) : Promise<User>{
+    return this.usersService.findOne({id : id});
+  }
+
+  @Patch(':id')
+  async update(@Param('id') id: string, @Body() 
+    UserData: {name: string, email:string}) : Promise<User> {
+      const {name, email} = UserData
+    return this.usersService.update({
+      where: {id: id},
+      data: {name, email}
+    });
+  }
+
+  @Delete(':id')
+  async remove(@Param('id') id: string) : Promise<User>{
+    return this.usersService.remove({
+      id: id
+    });
+  }
+}
