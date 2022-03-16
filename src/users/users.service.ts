4f91050193d1ac370e6a8a7578b1a41aa421bc90
Damhafi <yusufmilla@gmail.com>
2022-03-16 17:08:56 -0300

    att.

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -4,37 +4,39 @@ import { UpdateUserDto } from './dto/update-user.dto';
 import { PrismaService } from '../prisma.service';
 import { User, Prisma } from '@prisma/client';
 
+
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
-  async create(data: Prisma.UserCreateInput) : Promise<User> {
+  async create(data: Prisma.UserCreateInput): Promise<User> {
     return this.prisma.user.create({
-      data
-    })
+      data,
+    });
   }
 
-  async findAll() : Promise<User[] | null> {
+  async findAll(): Promise<User[] | null> {
     return this.prisma.user.findMany();
   }
 
-  async findOne(id: Prisma.UserWhereUniqueInput) : Promise<User | null>{
+  async findOne(id: Prisma.UserWhereUniqueInput): Promise<User | null> {
     return this.prisma.user.findUnique({
-      where: id
+      where: id,
     });
   }
 
   async update(params: {
-    where : Prisma.UserWhereUniqueInput;
-    data : Prisma.UserUpdateInput;
+    where: Prisma.UserWhereUniqueInput;
+    data: Prisma.UserUpdateInput;
   }) {
     const { where, data } = params;
     return this.prisma.user.update({
-      data, where
+      data,
+      where,
     });
   }
 
-  async remove(where : Prisma.UserWhereUniqueInput) : Promise<User> {
-    return this.prisma.user.delete({where})
+  async remove(where: Prisma.UserWhereUniqueInput): Promise<User> {
+    return this.prisma.user.delete({ where });
   }
 }
